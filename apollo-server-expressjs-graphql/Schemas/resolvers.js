12ad09a30d73151568acae74d99eb2087550a511
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-09-04 11:11:00 +0530

    updated

--- apollo-server-expressjs-graphql/Schemas/resolvers.js
+++ apollo-server-expressjs-graphql/Schemas/resolvers.js
@@ -6,20 +6,52 @@ export const resolvers = {
   Query: {
     hello: () => "Hello World!",
     age: () => 20,
-    posts: async() => {
+    posts: async () => {
       // you can connect to third party rest api endpoints or db
       // Let's connect to  third party rest api
-      const result = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      const result = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      console.log(result.data);
+      return result.data;
+    },
+    postById: async (parent, args) => {
+      console.log(parent);
+      console.log(args); // receive the args object
+      // Let's connect to  third party rest api
+      const result = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts/" + args.id
+      );
       console.log(result.data);
       return result.data;
     },
     users: async () => {
-      // // exec db query as per mongoose syntax 
+      // // exec db query as per mongoose syntax
       // const result = await Users.find();
       // console.log("========FOUND USERS =========");
       // console.log(result);
-      // // get the data and return 
+      // // get the data and return
       // return result;
-    }
+    },
+  },
+  Mutation: {
+    createPost: async (parent, args) => {
+      console.log(args);
+      const result = await axios.post(
+        "https://jsonplaceholder.typicode.com/posts",
+        args // form data
+      );
+      console.log(result.data);
+      return result.data;
+    },
+    updatePost: async (parent, args) => {
+      console.log(args);
+      const result = await axios.put(
+        "https://jsonplaceholder.typicode.com/posts/" + args.id,
+        args // form data
+      );
+      console.log(result.data);
+      return result.data;
+    },
   },
 };
